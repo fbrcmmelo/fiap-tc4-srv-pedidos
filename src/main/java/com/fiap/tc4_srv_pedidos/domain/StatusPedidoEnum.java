8050e47f4ff368607a1c5667af3e0601fa568a5e
fbrcm <fbrc.mmlelo@gmail.com.br>
2025-05-21 23:24:12 -0300

    feat: inserido status fechado sem credito

--- src/main/java/com/fiap/tc4_srv_pedidos/domain/StatusPedidoEnum.java
+++ src/main/java/com/fiap/tc4_srv_pedidos/domain/StatusPedidoEnum.java
@@ -2,5 +2,9 @@ package com.fiap.tc4_srv_pedidos.domain;
 
 public enum StatusPedidoEnum {
 
-    ABERTO, FECHADO_COM_SUCESSO, FECHADO_SEM_SUCESSO, SEM_ESTOQUE
+    ABERTO,
+    FECHADO_COM_SUCESSO,
+    FECHADO_SEM_SUCESSO,
+    SEM_ESTOQUE,
+    FECHADO_SEM_CREDITO
 }
